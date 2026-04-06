3efd5f5363a0f2704595f4c22f6df82de7949cdf
pnwkw <90510234+pnwkw@users.noreply.github.com>
2026-04-06 20:39:24 +0200

    Fix rust-analyzer error on include_bytes!() call

--- crates/poedata_db/src/lib.rs
+++ crates/poedata_db/src/lib.rs
@@ -3,7 +3,7 @@ use std::sync::LazyLock;
 use poedata::data::Data;
 
 pub static DATA: LazyLock<Data> = LazyLock::new(|| {
-    let bytes = include_bytes!("poedata.cbor");
+    let bytes = include_bytes!("./poedata.cbor");
     serde_cbor_2::from_reader(&bytes[..]).unwrap()
 });
 
